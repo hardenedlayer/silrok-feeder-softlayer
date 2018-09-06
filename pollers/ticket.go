9097e07deb54502ba31e12dd65ee62fddf45e688
Yonghwan SO <scinix@gmail.com>
2018-09-06 16:30:10 +0900

    fixed date setting method

--- pollers/ticket.go
+++ pollers/ticket.go
@@ -37,35 +37,44 @@ func (p *TicketPoller) Run(in chan srfsoftlayer.Message) (chan srfsoftlayer.Mess
 }
 
 func (p *TicketPoller) run(service services.Brand, in, out chan srfsoftlayer.Message) {
+	//! in SL API, all date values should be represented as US/Central. @.@
 	loc, _ := time.LoadLocation("US/Central")
-	dateNow := time.Now().AddDate(0, 0, 0).In(loc)
+
+	// initial start date for between filter: 10s before starting.
+	dateNow := time.Now().Add(-10 * time.Second)
 	if p.FetchAll {
-		dateNow = time.Now().AddDate(-3, -1, 0).In(loc)
+		dateNow = time.Now().AddDate(-3, 0, 0)
 	}
-	dateStartStr := dateNow.Format("01/02/2006 15:04:05")
+	dateLast := dateNow
+	dateStartStr := dateNow.In(loc).Format("01/02/2006 15:04:05")
+
 	retry := false
 	for {
 		select {
 		case _, ok := <-in:
 			if !ok {
 				return
-
 			}
 		default:
 			if p.FetchAll {
 				if !retry { // for fetching all mode, add 5 days per iteration.
 					dateNow = dateNow.AddDate(0, 0, 5)
 				} else {
+					// shorten period. sometimes api call fails when too many tickets.
 					dateNow = dateNow.AddDate(0, 0, -2)
+					retry = false
 				}
-				if dateNow.After(time.Now().In(loc)) {
-					dateNow = time.Now().In(loc)
+
+				if dateNow.After(time.Now()) {
+					dateNow = time.Now()
 					p.FetchAll = false
 				}
-			} else {
-				dateNow = time.Now().In(loc)
+			} else { // end date for between filter
+				dateNow = time.Now()
 			}
-			dateEndStr := dateNow.Format("01/02/2006 15:04:05")
+
+			dateStartStr = dateLast.Add(1 * time.Second).In(loc).Format("01/02/2006 15:04:05")
+			dateEndStr := dateNow.In(loc).Format("01/02/2006 15:04:05")
 			//! for DateBetween, entiries exactly matched with start and end date also included
 			data, err := service.
 				Mask("attachedVirtualGuests.id;attachedVirtualGuests.hostname;attachedVirtualGuests.domain;attachedVirtualGuests.typeId;attachedVirtualGuests.location.pathString;attachedVirtualGuests.tagReferences;attachedHardware.id;attachedHardware.hostname;attachedHardware.domain;attachedHardware.location.pathString;attachedHardware.tagReferences;id;accountId;assignedUserId;groupId;createDate;lastEditDate;lastEditType;lastResponseDate;locationId;modifyDate;priority;responsibleBrandId;statusId;subjectId;title;firstUpdate.editorType;firstUpdate.editorId;status").
@@ -75,29 +84,32 @@ func (p *TicketPoller) run(service services.Brand, in, out chan srfsoftlayer.Mes
 				GetTickets()
 
 			if err != nil {
-				fmt.Printf("ERROR on API call!")
+				fmt.Printf("ERROR on API call! sleep 5s and continue.")
 				srfsoftlayer.Inspect("ERROR", err)
 				time.Sleep(5 * time.Second)
 				retry = true
 				continue
-			} else {
-				for _, ticket := range data {
-					if ticket.Title == nil {
-						fmt.Printf("OOPS! title is nil\n")
-						srfsoftlayer.Inspect("ERROR", ticket)
-					} else {
-						out <- srfsoftlayer.Message{
-							Type:    "ticket",
-							Title:   *ticket.Title,
-							Content: ticket,
-						}
-					}
-				}
-				fmt.Printf("total open tickets: %v from %v to %v\n", len(data), dateStartStr, dateEndStr)
-				dateStartStr = dateNow.Add(time.Second).Format("01/02/2006 15:04:05")
-				time.Sleep(5 * time.Second)
-				retry = false
 			}
+
+			fmt.Printf("total open tickets: %v from %v to %v\n", len(data), dateStartStr, dateEndStr)
+			for _, ticket := range data {
+				if ticket.Title == nil {
+					fmt.Printf("OOPS! title is nil\n")
+					srfsoftlayer.Inspect("ERROR", ticket)
+				} else {
+					out <- srfsoftlayer.Message{
+						Type:    "ticket",
+						Title:   *ticket.Title,
+						Content: ticket,
+					}
+					fmt.Printf("- ticket #%v create: %v (last: %v", *ticket.Id, ticket.CreateDate.Time, dateLast)
+					if ticket.CreateDate.Time.After(dateLast) {
+						dateLast = ticket.CreateDate.Time
+					}
+					fmt.Printf(" -> %v\n", dateLast)
+				}
+			}
+			time.Sleep(5 * time.Second)
 		}
 	}
 }
